44ea80220d9a86a169baed55786a5f9c2b5223ea
thorneenroht <mattst9785@yahoo.com>
2016-01-01 20:54:48 -0500

    I changed the layout of the page and added text.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,15 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'M T';
+  $scope.hello = 'Hi, I\'m Matt.';
+  $scope.msg = 'I am a software engineer.';
+  $scope.email = 'matthew.thorne.s@gmail.com';
+  $scope.github = 'https://github.com/thorneenroht'; 
   $scope.img = 'imgs/folioImage.jpg';
-  
+  $scope.linkedin = 'https://www.linkedin.com/in/mattsthorne';
+  $scope.description = 'I spent time at two internships prior to graduation I worked at OmniTechnology Professionals and ApartmentSmart.com.  While at my internships I worked with html, css, javascript, jquery, jquery ui, phonegap.  I currently work at a Java development shop where I focus primarily on developing reliable middle ware systems.';
+  $scope.description2 = 'I have agile experience specifically with scrum methodology.  I am comfortable working in teams and rather enjoy aspects of working in a team that agile can leverage such as pair programming.';
+  $scope.below = 'Check out my activity below...';
+
+
+
 }]);
